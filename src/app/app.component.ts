7c9ae7d4df8c355e5119719b3c71268621f8dba2
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-06-13 23:01:22 +0200

    Adding modele and mock pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list'; // on importe nos listes de pokémons contenant toute les données
+import { Pokemon } from './pokemon'; // permet d'importer le modèle
 
 @Component({
   selector: 'app-root',
-  template: `
-      <h1>
-        Welcome to {{title}}!
-      </h1>`
+  template: `<h1>Liste de Pokémons</h1>`
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit{
+  pokemonList : Pokemon [] = POKEMONS
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]) //appelle la méthode
+  }
+
+  selectPokemon(pokemon : Pokemon){
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+    
+  } // déclare une méthode 
 }
