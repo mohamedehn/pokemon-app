0c6ca2ee8ffec3a77c3874df7409bf5944c7e914
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2023-10-20 23:05:14 +0200

    adding custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,23 @@ import { Pokemon } from './pokemon'; // permet d'importer le modèle
   templateUrl: 'app.component.html' //sépare le template (vue) de la logique (typescript pour faire tourner le component), sur un fichier dédié
 })
 export class AppComponent implements OnInit{
-  pokemonList : Pokemon [] = POKEMONS
+  pokemonList : Pokemon [] = POKEMONS;
+  pokemonSelected : Pokemon | undefined ;
 
   ngOnInit() {
     console.table(this.pokemonList);
   }
 
-  selectPokemon(event : MouseEvent){
-    const index : number = +(event.target as HTMLInputElement).value // permet de travailler dans la class du component
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId : string){
+    // permettra l'affichage côté utilisateur plus tôt que côté dev, on utilise la méthode find pour vérifier si le pokemon a un ID
+    const pokemon : Pokemon | undefined  = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon){ //on vérifie si le pokemon est trouvé
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`); // dans ce cas on renvoi le nom du pokemon
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`Vous avez demandé un pokemon qui n'existe pas`); // Sinon on indique qu'on ne trouve pas
+      this.pokemonSelected = pokemon;
+    }
     
   } // déclare une méthode qui permet d'intéragir directement avec les évènements qu'aura entrer l'utilisateur dans le input du template
 }
