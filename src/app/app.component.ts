5c5cc17a03f3ee4b2f96318f14465e0ff3230cf3
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2023-10-22 19:56:39 +0200

    Starting router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list'; // on importe nos listes de pokémons contenant toute les données
-import { Pokemon } from './pokemon'; // permet d'importer le modèle
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html' //sépare le template (vue) de la logique (typescript pour faire tourner le component), sur un fichier dédié
 })
-export class AppComponent implements OnInit{
-  pokemonList : Pokemon [] = POKEMONS;
-  pokemonSelected : Pokemon | undefined ;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId : string){
-    // permettra l'affichage côté utilisateur plus tôt que côté dev, on utilise la méthode find pour vérifier si le pokemon a un ID
-    const pokemon : Pokemon | undefined  = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if(pokemon){ //on vérifie si le pokemon est trouvé
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`); // dans ce cas on renvoi le nom du pokemon
-      this.pokemonSelected = pokemon;
-    }else{
-      console.log(`Vous avez demandé un pokemon qui n'existe pas`); // Sinon on indique qu'on ne trouve pas
-      this.pokemonSelected = pokemon;
-    }
-    
-  } // déclare une méthode qui permet d'intéragir directement avec les évènements qu'aura entrer l'utilisateur dans le input du template
+export class AppComponent{
+  
 }
