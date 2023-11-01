bcac46637078425c55b44b8f903a07795bbd6a94
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2023-11-01 20:27:36 +0100

    edit form pokemon done

---
+++ src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-pokemon',
+  template: `
+   <h2 class="center">Editer {{pokemon?.name}}</h2>
+   <p *ngIf="pokemon" class="center">
+      <img [src]="pokemon.picture" alt="">
+   </p>
+   <app-pokemon-form *ngIf="pokemon" [pokemon]="pokemon"></app-pokemon-form>
+  `,
+  styles: [
+  ]
+})
+export class EditPokemonComponent implements OnInit{
+
+  pokemon: Pokemon | undefined ;
+
+  constructor(
+    private pokemonService: PokemonService, 
+    private route: ActivatedRoute)
+    {}
+
+  ngOnInit(): void {
+    
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if(pokemonId){
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+    }else{
+      this.pokemon = undefined;
+    }
+  }
+}
--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-pokemon-form',
   templateUrl: './pokemon-form.component.html',
+  styleUrls: ['./pokemon-form.component.css'],
   styles: [
   ]
 })
@@ -36,7 +37,15 @@ export class PokemonFormComponent implements OnInit{
   }
 
   isTypesValid(type: string): boolean {
-    return this.pokemon.types.includes(type)
+    //si une seule case coché on bloque la possibilité de décocher la seule case coché
+    if(this.pokemon.types.length == 1 && this.hasType(type)){
+      return false;
+    }
+    //si l'utilisateur à coché plus de 2 cases (donc 3), on le bloque car on autorisera 3 types maximum
+    if(this.pokemon.types.length > 2 && !this.hasType(type)){
+      return false;
+    }
+    return true;
   }
 
   onSubmit(){
